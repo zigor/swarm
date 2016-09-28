e477596f97d2c6e047ee863210e434b2dc278506
zigor <zhukovsky.igor@me.com>
2016-09-28 14:30:29 +0200

    add experiment files

---
+++ Swarn/Scripts/randoms.js
@@ -0,0 +1,15 @@
+﻿function random() {
+    return {
+        onSphere: function (radius) {
+            var angle = Math.random() * Math.PI * 2;
+            var unitVector = Math.random() * 2 - 1;
+
+            return new THREE.Vector3(
+                Math.cos(angle) * Math.sqrt(1 - Math.pow(unitVector, 2)) * radius,
+                Math.sin(angle) * Math.sqrt(1 - Math.pow(unitVector, 2)) * radius,
+                unitVector * radius
+            );
+        }
+    }
+}
+
---
+++ Swarn/Scripts/sample.js
@@ -0,0 +1,39 @@
+﻿function Sample(count) {
+    this.count = count;
+    this.positions = new Float32Array(count * 3);
+    this.colors = new Float32Array(count * 3);    
+    this.positionRule = null;  
+    this.colorRule = null;  
+};
+
+Sample.prototype = {
+
+    setPositionRule: function (producePositionRule) {
+        this.positionRule = producePositionRule;
+        return this;
+    },
+
+    setColorRule: function(produceColorRule) {
+        this.colorRule = produceColorRule;
+        return this;
+    },
+
+    refresh: function() {
+        
+        for (var i = 0; i < this.count; i += 3) {
+
+            var v3 = this.positionRule();
+            
+            this.positions[i] = v3.x;
+            this.positions[i + 1] = v3.y;
+            this.positions[i + 2] = v3.z;
+
+            var rgb = this.colorRule(v3);
+
+            this.colors[i] = rgb.r;
+            this.colors[i + 1] = rgb.g;
+            this.colors[i + 2] = rgb.b;
+        }
+    }
+}
+
