acf418a6e939dd9750d7c24f3a0099547f0810d9
zigor <zhukovsky.igor@me.com>
2016-10-07 16:04:47 +0200

    make swarm fly

--- Swarn/Scripts/sample.js
+++ Swarn/Scripts/sample.js
@@ -1,39 +1,59 @@
-﻿function Sample(count) {
-    this.count = count;
-    this.positions = new Float32Array(count * 3);
-    this.colors = new Float32Array(count * 3);    
-    this.positionRule = null;  
-    this.colorRule = null;  
-};
+﻿function Sample(sampleCount) {
 
-Sample.prototype = {
+    var count = sampleCount;
+    var positions = new Float32Array(count * 3);
+    var colors = new Float32Array(count * 3);
 
-    setPositionRule: function (producePositionRule) {
-        this.positionRule = producePositionRule;
-        return this;
-    },
+    var positionRule = null;
+    var colorRule = null;
 
-    setColorRule: function(produceColorRule) {
-        this.colorRule = produceColorRule;
-        return this;
-    },
+    var zero = new THREE.Vector3(0, 0, 0);
+    var white = new THREE.Color().setRGB(1, 1, 1);   
 
-    refresh: function() {
-        
-        for (var i = 0; i < this.count; i += 3) {
+    var funcs = {
+        getPoints: function () {
+            return positions.slice(0);
+        },
 
-            var v3 = this.positionRule();
-            
-            this.positions[i] = v3.x;
-            this.positions[i + 1] = v3.y;
-            this.positions[i + 2] = v3.z;
+        getColors: function () {
+            return colors.slice(0);
+        },
 
-            var rgb = this.colorRule(v3);
+        setPositionRule: function (producePositionRule) {
 
-            this.colors[i] = rgb.r;
-            this.colors[i + 1] = rgb.g;
-            this.colors[i + 2] = rgb.b;
+            positionRule = producePositionRule;
+
+            for (var i = 0; i < count; i += 3) {
+
+                var v3 = positionRule();
+
+                positions[i] = v3.x;
+                positions[i + 1] = v3.y;
+                positions[i + 2] = v3.z;
+            }
+
+            return this;
+        },
+
+        setColorRule: function (produceColorRule) {
+            colorRule = produceColorRule;
+
+            for (var i = 0; i < count; i += 3) {
+
+                var rgb = colorRule(new THREE.Vector3(positions[i], positions[i + 1], positions[i + 2]));
+
+                colors[i] = rgb.r;
+                colors[i + 1] = rgb.g;
+                colors[i + 2] = rgb.b;
+            }
+
+            return this;
         }
     }
-}
 
+    funcs.setPositionRule(function () { return zero });
+
+    funcs.setColorRule(function () { return white });
+
+    return funcs;
+};
--- Swarn/Scripts/swarm.js
+++ Swarn/Scripts/swarm.js
@@ -1,50 +1,111 @@
-        var container;
-        var camera, scene, renderer;
-        var points;
-        var radius = 200;
-        init();
-        animate();
-        function init() {
-            container = document.getElementById('container');
-            camera = new THREE.PerspectiveCamera(27, window.innerWidth / window.innerHeight, 5, 3500);
-            camera.position.z = 2750;
-            scene = new THREE.Scene();
-            scene.fog = new THREE.Fog(0x050505, 2000, 3500);
-            var geometry = new THREE.BufferGeometry();
-   
-            var sample = new Sample(10000);
-            sample.setPositionRule(function () {
-                return random.inEllipsoid(radius - 100 + Math.random() * radius, 500, 200);
-            }).setColorRule(function (v3) {
-                
-                return  new THREE.Color().setRGB(1, 1, 1);
-            }).refresh();
-            
-            geometry.addAttribute('position', new THREE.BufferAttribute(sample.positions, 3));
-            geometry.addAttribute('color', new THREE.BufferAttribute(sample.colors, 3));
-            geometry.computeBoundingSphere();
-            var material = new THREE.PointsMaterial({ size: 15, vertexColors: THREE.VertexColors });
-            points = new THREE.Points(geometry, material);
-            scene.add(points);
-            renderer = new THREE.WebGLRenderer({ antialias: false });
-            renderer.setClearColor(scene.fog.color);
-            renderer.setPixelRatio(window.devicePixelRatio);
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            container.appendChild(renderer.domElement);
-            window.addEventListener('resize', onWindowResize, false);
-        }
-        function onWindowResize() {
+function Swarm(container, count) {
+
+    var camera, scene, renderer;
+
+    var points;
+
+    var sample = new Sample(count);
+
+    var getGeometry = function () {
+        var geometry = new THREE.BufferGeometry();
+
+        geometry.addAttribute('position', new THREE.BufferAttribute(sample.getPoints(), 3));
+        geometry.addAttribute('color', new THREE.BufferAttribute(sample.getColors(), 3));
+        geometry.computeBoundingSphere();
+        var material = new THREE.PointsMaterial({ size: 15, vertexColors: THREE.VertexColors });
+        points = new THREE.Points(geometry, material);
+        scene.add(points);
+    };
+
+    var prepareCamera = function () {
+        camera = new THREE.PerspectiveCamera(27, window.innerWidth / window.innerHeight, 5, 3500);
+        camera.position.z = 2750;
+    }
+
+    var prepareScene = function () {
+        scene = new THREE.Scene();
+        scene.fog = new THREE.Fog(0x050505, 2000, 3500);
+
+        getGeometry();
+    }
+
+    var prepareRenderer = function () {
+
+        renderer = new THREE.WebGLRenderer({ antialias: false });
+        renderer.setClearColor(scene.fog.color);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    var init = function () {
+
+        prepareCamera();
+        prepareScene();
+        prepareRenderer();
+
+        container.appendChild(renderer.domElement);
+
+        window.addEventListener('resize', function () {
+
             camera.aspect = window.innerWidth / window.innerHeight;
             camera.updateProjectionMatrix();
             renderer.setSize(window.innerWidth, window.innerHeight);
+
+        }, false);
+    };
+
+    var render = function () {
+        var time = Date.now() * 0.001;
+        points.rotation.x = time * 0.25;
+        points.rotation.y = time * 0.5;
+
+        TWEEN.update();
+
+        renderer.render(scene, camera);
+    };
+
+    var animate = function () {
+        requestAnimationFrame(animate);
+        render();
+    };
+
+    init();
+
+    animate();
+
+    return {
+        to: function (positionRule, colorRule, duration, onComplete) {
+
+            if (positionRule != null) {
+                sample.setPositionRule(positionRule);
+            }
+
+            if (colorRule) {
+                sample.setPositionRule(colorRule);
+            }
+
+            var toPoints = sample.getPoints();
+            var fromPoints = points.geometry.attributes.position.array.slice(0);
+
+            new TWEEN.Tween(points.geometry.attributes)
+                .to({}, duration)
+                .onUpdate(function (step) {
+
+                    for (var i = 0; i < this.position.array.length; ++i) {
+                        this.position.array[i] = fromPoints[i] + (toPoints[i] - fromPoints[i]) * step;
+                    }
+
+                    this.position.updateRange.count = this.position.array.length;
+
+                    if (this.position.updateRange.count > 0) {
+                        this.position.needsUpdate = true;
+                    }
+                })
+                .easing(TWEEN.Easing.Bounce.Out)
+                .onComplete(function () { onComplete(); })
+                .start();
+
+            return this;
         }
-        function animate() {
-            requestAnimationFrame(animate);
-            render();
-        }
-        function render() {
-            var time = Date.now() * 0.001;
-            points.rotation.x = time * 0.25;
-            points.rotation.y = time * 0.5;
-            renderer.render(scene, camera);
-        }
+    };
+}
